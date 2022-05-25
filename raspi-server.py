4089e188b6b16f35a24f4e724e137af7001f948a
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-05-24 22:16:24 -0400

    Modified raspi-server.py

--- raspi-server.py
+++ raspi-server.py
@@ -1,16 +1,18 @@
 import socket
+import sys
 
-port = 3
+raspi_add = "e4:70:b8:d3:d7:c1"
+port = 30 if len(sys.argv) == 1 else int(sys.argv[1])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TODO Remove
-# s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((socket.gethostname(), port))  # TODO Revert
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.bind((raspi_add, port))
 s.listen(1)
 
+print(f"raspi_add: {raspi_add}")
 print("Waiting for connection...")
 
 client, address = s.accept()
-print(f"Connected to {client} on address {address}")
+print(f"Connected on address {address}")
 
 while True:
     data = client.recv(1024)
