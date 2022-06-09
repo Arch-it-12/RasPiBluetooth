074c9a2001b813df2934fe8da512623e368ae91a
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-06-09 14:46:49 -0400

    Added video stream alicetag detection

--- TagDetection/detector.py
+++ TagDetection/detector.py
@@ -1,24 +1,46 @@
+import sys
+
 import cv2
 import imutils
+from imutils.video import VideoStream
 
-image = cv2.imread("image.png")
-image = imutils.resize(image, width=600)
+STREAM = VideoStream(src=0).start()
+ARUCO_DICT = cv2.aruco.Dictionary_get(cv2.aruco.DICT_APRILTAG_36h11)
+ARUCO_PARAMS = cv2.aruco.DetectorParameters_create()
 
-arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_APRILTAG_36h11)
-arucoParams = cv2.aruco.DetectorParameters_create()
-(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
-                                                   parameters=arucoParams)
-(ptA, ptB, ptC, ptD) = corners[0][0]
-ptB = (int(ptB[0]), int(ptB[1]))
-ptC = (int(ptC[0]), int(ptC[1]))
-ptD = (int(ptD[0]), int(ptD[1]))
-ptA = (int(ptA[0]), int(ptA[1]))
+while True:
+    frame = STREAM.read()
+    frame = imutils.resize(frame, width=1000)
 
-# draw the bounding box of the AprilTag detection
-cv2.line(image, ptA, ptB, (0, 255, 0), 2)
-cv2.line(image, ptB, ptC, (0, 255, 0), 2)
-cv2.line(image, ptC, ptD, (0, 255, 0), 2)
-cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, ARUCO_DICT, parameters=ARUCO_PARAMS)
 
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+    if len(corners) >= 1:
+        for c, marker_id in zip(corners, ids):
+            (ptA, ptB, ptC, ptD) = c.reshape((4, 2))
+
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+
+            # draw the bounding box of the AprilTag detection
+            cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
+
+            cv2.line(frame, (abs(int((ptA[0] + ptB[0]) / 2)), abs(int((ptA[1] + ptB[1]) / 2))),
+                     (abs(int((ptC[0] + ptD[0]) / 2)), abs(int((ptC[1] + ptD[1]) / 2))),
+                     (0, 255, 0), 2)
+
+            cv2.line(frame, (abs(int((ptA[0] + ptD[0]) / 2)), abs(int((ptA[1] + ptD[1]) / 2))),
+                     (abs(int((ptB[0] + ptC[0]) / 2)), abs(int((ptB[1] + ptC[1]) / 2))),
+                     (0, 255, 0), 2)
+
+            cv2.putText(frame, str(marker_id), (ptA[0], ptD[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow("Camera Feed", frame)
+    x = cv2.waitKey(1)
+
+    if x == ord("q"):
+        sys.exit()
