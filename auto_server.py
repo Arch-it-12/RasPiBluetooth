0ec1db514393748f57e6061ba75c5c5ae28e32b3
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-06-13 00:53:41 -0400

    Stuff

--- auto_server.py
+++ auto_server.py
@@ -5,51 +5,88 @@ import imutils
 from imutils.video import VideoStream
 
 from methods import pyth, points, midpoint, center
+from motor_server import ultrasonic, left, right, up, down, forward, no_movement, bot_off
 
 # ArUco Constants
 STREAM = VideoStream(src=0).start()
 ARUCO_DICT = cv2.aruco.Dictionary_get(cv2.aruco.DICT_APRILTAG_36h11)
 ARUCO_PARAMS = cv2.aruco.DetectorParameters_create()
 
-# Distance Constants
-ACTUAL_LENGTH = 10.5  # Actual length of the side of the april tag (cm)
-TEST_DISTANCE = 100  # Distance from which the measurement was taken (cm)
-PIXEL_LENGTH_INBUILT = 85
-PIXEL_LENGTH_EXTERNAL = 113  # Length of the side in pixels at TEST_DISTANCE cm away (px)
+TARGET_TAG = 423
+TARGET_PX_DISTANCE = 655
 
-FOCAL_LENGTH = (PIXEL_LENGTH_INBUILT * TEST_DISTANCE) / ACTUAL_LENGTH
-FW = ACTUAL_LENGTH * FOCAL_LENGTH
+frame = imutils.resize(STREAM.read(), width=1000)
+FRAME_CENTER = (int(frame.shape[1] / 2), int(frame.shape[0] / 2))
 
-while True:
+flag = True
+is_turning = False
+
+while flag:
     frame = STREAM.read()
     frame = imutils.resize(frame, width=1000)
 
     (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, ARUCO_DICT, parameters=ARUCO_PARAMS)
 
-    midY = int(frame.shape[0] / 2)
-    midX = int(frame.shape[1] / 2)
+    cv2.rectangle(frame, (FRAME_CENTER[0] - 150, FRAME_CENTER[1] + 150), (FRAME_CENTER[0] + 150, FRAME_CENTER[1] - 150),
+                  (255, 0, 0), 2)
+    cv2.aruco.drawDetectedMarkers(frame, corners, borderColor=(255, 255, 0))
 
-    cv2.rectangle(frame, (midX - 200, midY + 200), (midX + 200, midY - 200), (255, 0, 0), 2)
-    cv2.circle(frame, (midX, midY), 2, (255, 0, 0), -1)
+    cv2.circle(frame, FRAME_CENTER, 3, (255, 0, 0), -1)
 
     if len(corners) >= 1:
         for c, marker_id in zip(corners, ids):
             ptA, ptB, ptC, ptD = points(c)
             mp = midpoint(ptA, ptC)
 
-            side_length = max(pyth(ptA, ptB), pyth(ptB, ptC), pyth(ptC, ptD), pyth(ptD, ptA))
-            measured_distance_cm = FW / side_length
+            cv2.circle(frame, center(ptA, ptC, ptB, ptD), 3, (0, 255, 0), -1)  # Center of tag
+            cv2.circle(frame, ptA, 3, (0, 0, 255), -1)  # Top left corner of tag
 
-            # draw the bounding box of the AprilTag detection
-            cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
-            cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
-            cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
-            cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
-            cv2.circle(frame, center(ptA, ptC, ptB, ptD), 2, (0, 255, 0), 2)
+            if marker_id != TARGET_TAG:
+                continue
 
-            cv2.putText(frame, str(marker_id), (ptA[0], ptD[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            cv2.putText(frame, "%.2fm" % (measured_distance_cm / 100), (ptB[0], ptC[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 255, 0), 1)
+            in_range = mp[0] in range(FRAME_CENTER[0] - 150, FRAME_CENTER[0] + 151) and mp[1] in range(
+                FRAME_CENTER[1] - 150,
+                FRAME_CENTER[1] + 159)
+
+            if mp[0] in range(FRAME_CENTER[0] - 15, FRAME_CENTER[0] + 16) and mp[1] in range(FRAME_CENTER[1] - 15,
+                                                                                             FRAME_CENTER[1] + 16):
+                is_turning = False
+                ultrasonic.threshold = ultrasonic.distance
+                ultrasonic.when_in_range = up
+                ultrasonic.when_out_of_range = bot_off
+
+            if in_range and not is_turning:
+                print("forward")
+                forward()
+            elif not in_range or is_turning:
+                is_turning = True
+                ultrasonic.when_in_range = None
+                ultrasonic.when_out_of_range = None
+
+                if mp[0] >= FRAME_CENTER[0] + 15:
+                    print("right")
+                    right()
+                elif mp[0] <= FRAME_CENTER[0] - 15:
+                    print("left")
+                    left()
+
+                if mp[1] >= FRAME_CENTER[1] + 15:
+                    print("down")
+                    down()
+                elif mp[1] <= FRAME_CENTER[1] - 15:
+                    print("up")
+                    up()
+
+            if max(pyth(ptA, ptB), pyth(ptB, ptC), pyth(ptC, ptD), pyth(ptD, ptA)) > TARGET_PX_DISTANCE:
+                print("arrived")
+                no_movement()
+
+                ultrasonic.threshold = ultrasonic.distance
+                ultrasonic.when_in_range = up
+                ultrasonic.when_out_of_range = bot_off
+
+                flag = False
+                break
 
     cv2.imshow("Camera Feed", frame)
     x = cv2.waitKey(1)
